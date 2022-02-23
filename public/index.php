85016b543d048318ffb5b3d347263b4a42bd51b4
Houssem TAYECH <houssem@forticas.com>
2022-02-23 10:05:00 +0100

    add php autoloader

--- public/index.php
+++ public/index.php
@@ -1,47 +1,48 @@
 <?php
 // TODO 1- définir un autoloader
 // TODO 2- Utilser les namespaces
-require_once __DIR__.'/../core/Controller.php';
-require_once __DIR__.'/../core/Dao.php';
-require_once __DIR__.'/../core/Model.php';
-require_once __DIR__.'/../model/Post.php';
-require_once __DIR__.'/../controller/PostController.php';
+spl_autoload_register(function ($class_name) {
+    $directories = ['controller', 'core', 'model'];
+    foreach ($directories as $directory) {
+        $filename = __DIR__ . '/../' . $directory . '/' . $class_name . '.php';
+        if (file_exists($filename))
+            include __DIR__ . '/../' . $directory . '/' . $class_name . '.php';
 
-
+    }
+});
 
 /**************************************/
 $request = $_SERVER['REQUEST_URI'];
 
 // TODO organiser la partie configuration
-$config = parse_ini_file(__DIR__."/../config.ini");
+$config = parse_ini_file(__DIR__ . "/../config.ini");
 
 define('BASE_URI', $config['base_uri']);
 
 
-
 // TODO creer un routeur
 
-switch ($request){
-    case BASE_URI.'/':
-    {
-        // afficher page d'accueil
+switch ($request) {
+    case BASE_URI . '/':
+        {
+            // afficher page d'accueil
 
-    }
+        }
         break;
-    case BASE_URI.'/posts':
-    {
+    case BASE_URI . '/posts':
+        {
 
-        $pc = new PostController();
-        $pc->list();
+            $pc = new PostController();
+            $pc->list();
 
-    }
+        }
         break;
-    case BASE_URI.'/post/':
-    {
-        // TODO remodifier le routeur
-        $pc = new PostController();
-        $pc->show($_GET['id']);
+    case BASE_URI . '/post/':
+        {
+            // TODO remodifier le routeur
+            $pc = new PostController();
+            $pc->show($_GET['id']);
 
-    }
+        }
         break;
 }
