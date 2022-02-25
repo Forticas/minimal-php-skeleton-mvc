3963decd21821bc95c33ac562d6d7a3887969f79
Houssem TAYECH <houssem@forticas.com>
2022-02-25 12:35:00 +0100

    Add possibility render without Layout

--- core/Controller.php
+++ core/Controller.php
@@ -1,19 +1,20 @@
 <?php
+declare(strict_types=1);
+
 namespace App\core;
 
 class Controller
 {
-
-    public function renderView(string $path, $args = []){
-
-            foreach ($args as $key => $value)
-            {
-                $$key = $value;
-            }
+    public function renderView(string $path, $args = [], bool $isWithoutLayout = false)
+    {
+        foreach ($args as $key => $value) {
+            $$key = $value;
+        }
         ob_start();
-        require_once __DIR__.'/../view/'.$path.'.php';
+        require_once __DIR__ . '/../view/' . $path . '.php';
         $content = ob_get_clean();
-        require_once __DIR__.'/../view/layout.php';
+        if (!$isWithoutLayout)
+            require_once __DIR__ . '/../view/layout.php';
     }
 
     public function redirectTo(string $path)
@@ -23,8 +24,7 @@ class Controller
 
     public function redirectToRoute(string $path)
     {
-        header("Location: {$_SERVER["REQUEST_SCHEME"]}://{$_SERVER["HTTP_HOST"]}".BASE_URI."/$path");
+        header("Location: {$_SERVER["REQUEST_SCHEME"]}://{$_SERVER["HTTP_HOST"]}" . BASE_URI . "/$path");
     }
 
-    // TODO définir les autres méthodes utiles
 }
